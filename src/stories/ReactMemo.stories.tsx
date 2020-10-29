6e2cbaa9bf2a4472e92ea48fbaacea0d9a13a98f
vovkahimik <vovkahimik@gmail.com>
2020-10-29 21:42:19 +0300

    React.memo wrapped UserSecret component

---
+++ src/stories/ReactMemo.stories.tsx
@@ -0,0 +1,40 @@
+import React, {useState} from "react";
+
+export default {
+    title: 'React.memo'
+}
+
+const NewMessagesCounter = (props: { count: number }) => {
+    return <div>
+        {props.count}
+    </div>
+}
+const UsersSecret = (props: { users: Array<string> }) => {
+    console.log('USERS')
+    return <div>{
+        props.users.map((user, index) =>
+            <div key={index}>
+                {user}-{index}
+            </div>
+        )
+    }</div>
+}
+const Users = React.memo(UsersSecret)
+
+export const Example1 = () => {
+    console.log('Example1')
+    const [counter, setCounter] = useState(-4)
+    const [users, setUsers] = useState(['user', 'user', 'user'])
+    const addUser = () => {
+        const newUsers = [...users, 'user']
+        setUsers(newUsers)
+    }
+    users.push('user') //добавляет user, но не отображает
+
+    return <>
+        <button onClick={() => setCounter(counter + 1)}>+1</button>
+        <button onClick={addUser}>+user</button>
+        <NewMessagesCounter count={counter}/>
+        <Users users={users}/>
+    </>
+}
