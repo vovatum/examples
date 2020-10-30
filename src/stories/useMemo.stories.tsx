eb4da9327ac6b98f1cd35566085bc102dcb0d468
vovkahimik <vovkahimik@gmail.com>
2020-10-30 17:04:51 +0300

    useMemo.stories.tsx show to interval with while

---
+++ src/stories/useMemo.stories.tsx
@@ -0,0 +1,32 @@
+import React, {useState} from "react";
+
+export default {
+    title: 'useMemo'
+}
+
+export const Example1 = () => {
+    const [a, setA] = useState(5)
+    const [b, setB] = useState(5)
+    let factorialA = 1
+    let factorialB = 1
+    for (let i = 1; i <= a; i++) {
+        let fake = 0;
+        while (fake < 700000000) {
+            fake++
+        }
+        factorialA *= i
+    }
+    for (let i = 1; i <= b; i++) {
+        factorialB *= i
+    }
+    return <>
+        <input value={a} onChange={(event) =>
+            setA(+event.currentTarget.value)}/>
+        <span>FactorialA ={factorialA}</span>
+        <div>
+            <input value={b} onChange={(event) =>
+                setB(+event.currentTarget.value)}/>
+            <span>FactorialB ={factorialB}</span>
+        </div>
+    </>
+}
