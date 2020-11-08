e7092b091623dcc13e261aa3e2d2938c3074d317
vovkahimik <vovkahimik@gmail.com>
2020-11-08 18:34:09 +0300

    20 - React кабзда как подробно: useEffect основы useEffect три варианта использования

--- src/stories/UseEffect.stories.tsx
+++ src/stories/UseEffect.stories.tsx
@@ -10,13 +10,21 @@ export const Example1 = () => {
     const [counter, setCounter] = useState(23)
 
     useEffect(() => {
-        // debugger
-        console.log('useEffect')
+        console.log('useEffect every render')
         document.title = counter.toString()
     })
+    useEffect(() => {
+        console.log('useEffect only first render(componentDidMount)')
+        document.title = counter.toString()
+    }, [])
+    useEffect(() => {
+        console.log('useEffect first render and every counter change')
+        document.title = counter.toString()
+    }, [counter])
 
     return <>
         <button onClick={() => setFake(fake + 1)}>+1</button>
-        {counter} {fake}
+        <button onClick={() => setCounter(counter + 1)}>+1</button>
+        counter - {counter} fake - {fake}
     </>
 }
