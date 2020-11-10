c2495922908d70e81f1ffd0585b2406efa5a5514
vovkahimik <vovkahimik@gmail.com>
2020-11-10 16:18:57 +0300

    21 - React кабзда как подробно: useEffect \ setTimeout \ setInterval don't job clock

---
+++ src/Componens/AnalogClock.stories.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import {AnalogClock} from "./AnalogClock";
+
+export default {
+    title: 'AnalogClock',
+    component: AnalogClock
+}
+
+export const AnalogClockExample = () => {
+    return <AnalogClock/>
+}
